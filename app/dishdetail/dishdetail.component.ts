75b39d3bae9e770b8e6822372f0b0a795556fdff
Lisa <lisa_martinez@dusty.tamiu.edu>
2018-02-04 11:41:58 -0600

    Animations and Gestures Part 2

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -10,8 +10,13 @@ import { Toasty } from 'nativescript-toasty';
 import 'rxjs/add/operator/switchMap';
 import { action } from "ui/dialogs";
 import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/modal-dialog";
-import { TextField } from 'ui/text-field';
 import { CommentComponent } from "../Comment/comment.component";
+import { Page } from "ui/page";
+import { Animation, AnimationDefinition } from "ui/animation";
+import { View } from "ui/core/view";
+import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
+import { Color } from 'color';
+import * as enums from "ui/enums";
 
 @Component({
   selector: 'app-dishdetail',
@@ -28,6 +33,12 @@ export class DishdetailComponent implements OnInit {
   numcomments: number;
   favorite: boolean = false;
 
+  showComments: boolean = false;
+
+  cardImage: View;
+  commentList: View;
+  cardLayout: View;
+
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
@@ -36,6 +47,7 @@ export class DishdetailComponent implements OnInit {
     private changeDetectorRef: ChangeDetectorRef,
     private modalService: ModalDialogService,
     private vcRef: ViewContainerRef,
+    private page: Page,
     @Inject('BaseURL') private BaseURL) { }
 
     ngOnInit() {
@@ -100,5 +112,96 @@ export class DishdetailComponent implements OnInit {
           }
         })
     }
+
+    onSwipe(args: SwipeGestureEventData) {
+
+       if (this.dish) {
+         this.cardImage = <View>this.page.getViewById<View>("cardImage");
+         this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+         this.commentList = <View>this.page.getViewById<View>("commentList");
+
+         if (args.direction === SwipeDirection.up && !this.showComments ) {
+           this.animateUp();
+         }
+         else if (args.direction === SwipeDirection.down && this.showComments ) {
+           this.showComments = false;
+           this.animateDown();
+         }
+       }
+
+     }
+
+     showAndHideComments() {
+         this.cardImage = <View>this.page.getViewById<View>("cardImage");
+         this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+         this.commentList = <View>this.page.getViewById<View>("commentList");
+
+         if (!this.showComments ) {
+           this.animateUp();
+         }
+         else if (this.showComments ) {
+           this.showComments = false;
+           this.animateDown();
+         }
+     }
+
+     animateUp() {
+       let definitions = new Array<AnimationDefinition>();
+       let a1: AnimationDefinition = {
+           target: this.cardImage,
+           scale: { x: 1, y: 0 },
+           translate: { x: 0, y: -200 },
+           opacity: 0,
+           duration: 500,
+           curve: enums.AnimationCurve.easeIn
+       };
+       definitions.push(a1);
+
+       let a2: AnimationDefinition = {
+           target: this.cardLayout,
+           backgroundColor: new Color("#ffc107"),
+           duration: 500,
+           curve: enums.AnimationCurve.easeIn
+       };
+       definitions.push(a2);
+
+       let animationSet = new Animation(definitions);
+
+       animationSet.play().then(() => {
+         this.showComments = true;
+       })
+       .catch((e) => {
+           console.log(e.message);
+       });
+     }
+
+     animateDown() {
+       let definitions = new Array<AnimationDefinition>();
+       let a1: AnimationDefinition = {
+           target: this.cardImage,
+           scale: { x: 1, y: 1 },
+           translate: { x: 0, y: 0 },
+           opacity: 1,
+           duration: 500,
+           curve: enums.AnimationCurve.easeIn
+       };
+       definitions.push(a1);
+
+       let a2: AnimationDefinition = {
+           target: this.cardLayout,
+           backgroundColor: new Color("#ffffff"),
+           duration: 500,
+           curve: enums.AnimationCurve.easeIn
+       };
+       definitions.push(a2);
+
+       let animationSet = new Animation(definitions);
+
+       animationSet.play().then(() => {
+       })
+       .catch((e) => {
+           console.log(e.message);
+       });
+     }
 
 }
