602d69edfc8adde620cd4eb414d419579052c74c
Lisa <lisa_martinez@dusty.tamiu.edu>
2018-02-04 21:53:08 -0600

    Sending Email

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject, ChangeDetectorRef } from '@angular/core';
-
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import * as Email from 'nativescript-email';
 import { DrawerPage } from '../shared/drawer/drawer.page';
 
 
@@ -14,8 +15,24 @@ export class ContactComponent extends DrawerPage {
 
 
   constructor(private changeDetectorRef:ChangeDetectorRef,
+    private fonticon: TNSFontIconService,
     @Inject('BaseURL') private BaseURL) {
       super(changeDetectorRef);
+    }
+
+    sendEmail() {
+      Email.available()
+        .then((avail: boolean) => {
+          if (avail) {
+            Email.compose({
+              to: ['confusion@food.net'],
+              subject: '[ConFusion]: Query',
+              body: 'Dear Sir/Madam:'
+            });
+          }
+          else
+            console.log('No Email Configured');
+        })
     }
 
 
